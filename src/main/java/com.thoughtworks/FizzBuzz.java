b133c3910e1172489202646d2d0750406f25ffef
unknown <1714407443@qq.com>
2020-06-15 15:55:33 +0800

    finish shuld_return_FizzBuzz_given_15_when_fizzPuzz

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -2,8 +2,14 @@ package com.thoughtworks;
 
 public class FizzBuzz {
     public String fizzBuzz(int num){
+        if(num % 3==0&&num % 5==0){
+            return "FizzBuzz";
+        }
         if(num % 3==0){
             return "Fizz";
+        }
+        if(num % 5==0){
+            return "Buzz";
         }
         return String.valueOf(num);
     }
--- src/test/java/com/thoughtworks/PratiaceTest.java
+++ src/test/java/com/thoughtworks/PratiaceTest.java
@@ -7,7 +7,7 @@ public class PratiaceTest {
     public void should_return_1_given_1_when_fizzPuzz(){
         int number=1;
         FizzBuzz fizzBuzz=new FizzBuzz();
-        String fizzBuzzResult= fizzBuzz.fizzBuzz(1);
+        String fizzBuzzResult= fizzBuzz.fizzBuzz(number);
         assertEquals("1",fizzBuzzResult);
     }
 
@@ -15,7 +15,22 @@ public class PratiaceTest {
     public void shuld_return_Fizz_given_3_when_fizzPuzz(){
         int number=3;
         FizzBuzz fizzBuzz=new FizzBuzz();
-        String fizzBuzzResult= fizzBuzz.fizzBuzz(3);
+        String fizzBuzzResult= fizzBuzz.fizzBuzz(number);
         assertEquals("Fizz",fizzBuzzResult);
+    }
+
+    @Test
+    public void shuld_return_Buzz_given_5_when_fizzPuzz(){
+        int number=5;
+        FizzBuzz fizzBuzz=new FizzBuzz();
+        String fizzBuzzResult= fizzBuzz.fizzBuzz(number);
+        assertEquals("Buzz",fizzBuzzResult);
+    }
+    @Test
+    public void shuld_return_FizzBuzz_given_15_when_fizzPuzz(){
+        int number=15;
+        FizzBuzz fizzBuzz=new FizzBuzz();
+        String fizzBuzzResult= fizzBuzz.fizzBuzz(number);
+        assertEquals("FizzBuzz",fizzBuzzResult);
     }
 }
