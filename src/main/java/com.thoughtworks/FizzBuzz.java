6c50d5bb907be9b519b01fdd743258ea5eb8ff33
unknown <1714407443@qq.com>
2020-06-15 15:46:14 +0800

    finish shuld_return_Fizz_given_3_when_fizzPuzz

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -2,6 +2,9 @@ package com.thoughtworks;
 
 public class FizzBuzz {
     public String fizzBuzz(int num){
+        if(num % 3==0){
+            return "Fizz";
+        }
         return String.valueOf(num);
     }
 }
--- src/test/java/com/thoughtworks/PratiaceTest.java
+++ src/test/java/com/thoughtworks/PratiaceTest.java
@@ -4,11 +4,18 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 public class PratiaceTest {
     @Test
-    public void should_return_1_given_1_when_input_number_1(){
+    public void should_return_1_given_1_when_fizzPuzz(){
         int number=1;
         FizzBuzz fizzBuzz=new FizzBuzz();
         String fizzBuzzResult= fizzBuzz.fizzBuzz(1);
-        assertEquals(fizzBuzzResult,"1");
+        assertEquals("1",fizzBuzzResult);
+    }
 
+    @Test
+    public void shuld_return_Fizz_given_3_when_fizzPuzz(){
+        int number=3;
+        FizzBuzz fizzBuzz=new FizzBuzz();
+        String fizzBuzzResult= fizzBuzz.fizzBuzz(3);
+        assertEquals("Fizz",fizzBuzzResult);
     }
 }
